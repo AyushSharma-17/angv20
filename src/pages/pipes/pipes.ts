2e6f019b9e870cdf644219fd7ca4baa34d230984
ayush <ayushsharma12345qwerty@gmail.com>
2025-08-25 03:34:05 +0530

    pipes1

--- src/pages/pipes/pipes.ts
+++ src/pages/pipes/pipes.ts
@@ -1,12 +1,20 @@
-import { UpperCasePipe } from '@angular/common';
+import { DatePipe, JsonPipe, LowerCasePipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pipes',
-  imports: [UpperCasePipe],
+  imports: [UpperCasePipe,JsonPipe,DatePipe,SlicePipe,LowerCasePipe,TitleCasePipe],
   templateUrl: './pipes.html',
   styleUrl: './pipes.css'
 })
 export class Pipes {
 firstname:string="ayush";
+courseName:string="angular pipe title";
+rollList=[1,2,3,4,5,6,7,8,9];
+currentDate :Date =new Date();
+studentList={
+  name:"ayush",
+  course:"angular",
+  roll:101
+}
 }
