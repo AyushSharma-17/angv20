2c2c97dffca1633e2b1014ca90079520b6102f0c
ayush <ayushsharma12345qwerty@gmail.com>
2025-09-06 13:59:21 +0530

    pipe new

--- src/pages/pipe-new-c/pipe-new-c.ts
+++ src/pages/pipe-new-c/pipe-new-c.ts
@@ -1,11 +1,25 @@
+import { DatePipe, JsonPipe, LowerCasePipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pipe-new-c',
-  imports: [],
+  imports: [UpperCasePipe,LowerCasePipe,TitleCasePipe,DatePipe,SlicePipe,JsonPipe],
   templateUrl: './pipe-new-c.html',
   styleUrl: './pipe-new-c.css'
 })
 export class PipeNewC {
 
+  courseName="Angular";
+  titleName="angular is a front end framework"
+  currentDate=new Date();
+  rollList:number[]=[1,2,3,4,5,6,7,8,9,10];
+  studentObj:any={
+    name:"John",
+    age:20,
+    course:"Angular",
+    address:{
+      city:"New York",
+      state:"NY"
+    }
+  }
 }
